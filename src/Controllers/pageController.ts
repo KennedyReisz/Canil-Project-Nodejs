bd601dc8059bf7c5a6fc5e366665ab9e5b9b979f
Kennedy <kennedyrecuperaodiscord@gmail.com>
2022-07-17 13:28:19 -0300

    Fixed Menu Bar

--- src/Controllers/pageController.ts
+++ src/Controllers/pageController.ts
@@ -2,6 +2,12 @@ import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
   res.render("Pages/PageView", {
+    menu: {
+      all: true,
+      dog: false,
+      cat: false,
+      fish: false,
+    },
     banner: {
       title: "Todos os animais",
       background: "allanimals.jpg",
@@ -11,6 +17,12 @@ export const home = (req: Request, res: Response) => {
 
 export const dogs = (req: Request, res: Response) => {
   res.render("Pages/PageView", {
+    menu: {
+      all: false,
+      dog: true,
+      cat: false,
+      fish: false,
+    },
     banner: {
       title: "Cachorros",
       background: "banner_dog.jpg",
@@ -20,6 +32,12 @@ export const dogs = (req: Request, res: Response) => {
 
 export const cats = (req: Request, res: Response) => {
   res.render("Pages/PageView", {
+    menu: {
+      all: false,
+      dog: false,
+      cat: true,
+      fish: false,
+    },
     banner: {
       title: "Gatos",
       background: "banner_cat.jpg",
@@ -29,6 +47,12 @@ export const cats = (req: Request, res: Response) => {
 
 export const fishes = (req: Request, res: Response) => {
   res.render("Pages/PageView", {
+    menu: {
+      all: false,
+      dog: false,
+      cat: false,
+      fish: true,
+    },
     banner: {
       title: "Peixes",
       background: "banner_fish.jpg",

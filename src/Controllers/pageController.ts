5a65ae1c86eca910c424f720e6505871d2dd3ed3
Kennedy <kennedyrecuperaodiscord@gmail.com>
2022-07-17 13:14:29 -0300

    Added Category

--- src/Controllers/pageController.ts
+++ src/Controllers/pageController.ts
@@ -1,20 +1,37 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-  res.render("Pages/PageView");
+  res.render("Pages/PageView", {
+    banner: {
+      title: "Todos os animais",
+      background: "allanimals.jpg",
+    },
+  });
 };
 
 export const dogs = (req: Request, res: Response) => {
-  res.send("dogs");
-  //res.render("pages/page")
+  res.render("Pages/PageView", {
+    banner: {
+      title: "Cachorros",
+      background: "banner_dog.jpg",
+    },
+  });
 };
 
 export const cats = (req: Request, res: Response) => {
-  res.send("cats");
-  //res.render("pages/page")
+  res.render("Pages/PageView", {
+    banner: {
+      title: "Gatos",
+      background: "banner_cat.jpg",
+    },
+  });
 };
 
 export const fishes = (req: Request, res: Response) => {
-  res.send("fishes");
-  //res.render("pages/page")
+  res.render("Pages/PageView", {
+    banner: {
+      title: "Peixes",
+      background: "banner_fish.jpg",
+    },
+  });
 };

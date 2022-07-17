166b5b1aed40890a559f175b5c58a26d7b8d109d
Kennedy <kennedyrecuperaodiscord@gmail.com>
2022-07-16 21:01:02 -0300

    Added Views

--- src/Controllers/pageController.ts
+++ src/Controllers/pageController.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-  res.send("home")
-  //res.render("pages/page")
+  res.render("Pages/PageView")
 }
 
 export const dogs = (req: Request, res: Response) => {

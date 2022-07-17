6efc0656b128720b29d0818df38807ce253ea120
Kennedy <kennedyrecuperaodiscord@gmail.com>
2022-07-17 15:39:20 -0300

    Fixed Redirect

--- src/Controllers/searchController.ts
+++ src/Controllers/searchController.ts
@@ -5,6 +5,11 @@ import { createMenuObject } from "../Helpers/createMenuObject";
 export const search = (req: Request, res: Response) => {
   let query: string = req.query.q as string;
 
+  if (!query) {
+    res.redirect("/");
+    return;
+  }
+
   let list = pet.getFromName(query);
 
   res.render("Pages/PageView", {
